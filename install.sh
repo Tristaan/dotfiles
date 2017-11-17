5293446c553c90369b9694ba133ed17602ba7200
Tristaan <tristan.sneider@gmail.com>
2017-11-17 04:54:41 +0100

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+SCRIPT_DIR=$(readlink -f ${0%/*})
+for var in "$@"
+do
+    if [[ $var == "-u"]]; then
+        sudo pacman -Syyu
+    fi
+    if [[ $var == "-ii"]]; then
+        sudo pacman -S wget
+        wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+        wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+        tar -xvf package-query.tar.gz &
+        tar -xvf yaourt.tar.gz
+        cd package-query
+        makepkg -sri
+        cd ../yaourt
+        makepkg -sri
+        cd ..
+        rm package-query* yaourt*
+
+        symlink_dotfiles()
+
+        sudo yaourt -S zsh zsh-syntax-highlighting i3blocks neovim i3-wm i3lock-color-git ctags
+    fi
+done
+function symlink_dotfiles {
+
+    ln -s .i3 $HOME/.i3
+    ln -s .vim $HOME/.config/nvim
+    ln -s .Xresources $HOME/.Xresources
+    ln -s .dmrc $HOME/.dmrc
+    for file in .config/* do
+        ln -s $file "${HOME}${file}"
+    done
+    ln -s .gitconfig $HOME/.gitconfig
+    ln -s .oh-my-zsh $HOME/.oh-my-zsh
+    ln -s .zshrc $HOME/.zshrc
+
+}
