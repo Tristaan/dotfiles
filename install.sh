549073569fc9018759c55cce5265fbb79e51fb38
Tristaan <tristan.sneider@gmail.com>
2017-11-17 06:03:39 +0100

    install script working as it should

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 SCRIPT_DIR=$(readlink -f ${0%/*})
+safe_link(){
+    local src="$1"
+    local dest="$HOME/`basename $src`"
+    [ ! -e "$dest" ] && ln -sf "$src" "$dest"
+}
+symlink_dotfiles() {
+
+    safe_link "$SCRIPT_DIR/.i3"
+    safe_link "$SCRIPT_DIR/.Xresources"
+    safe_link "$SCRIPT_DIR/.dmrc"
+    safe_link "$SCRIPT_DIR/.gitconfig"
+    safe_link "$SCRIPT_DIR/.oh-my-zsh"
+    safe_link "$SCRIPT_DIR/.zshrc"
+    for file in .config/*; do
+        ln -s "${SCRIPT_DIR}/${file}" "${HOME}/${file}"
+    done
+
+}
 for var in "$@"
 do
-    if [[ $var == "-u"]]; then
+    if [ $var == "-u" ] ; then
         sudo pacman -Syyu
     fi
-    if [[ $var == "-ii"]]; then
+
+    if [ $var == "-ii" ]; then
         sudo pacman -S wget
         wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
         wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
@@ -18,28 +37,11 @@ do
         cd ..
         rm package-query* yaourt*
         sudo yaourt -S zsh zsh-syntax-highlighting i3blocks neovim i3-wm i3lock-color-git ctags
-
-        symlink_dotfiles()
-
-    elif [[ $var == "-i" ]]; then
-        sudo yaourt -S megasync zsh zsh-syntax-highlighting i3blocks neovim i3-wm i3lock-color-git ctags
+        symlink_dotfiles
     fi
-    if [[ $var == "-l" ]]; then
-        symlink_dotfiles()
+
+    if [ $var == "-l" ]; then
+        symlink_dotfiles
 
     fi
 done
-function symlink_dotfiles {
-
-    ln -s .i3 $HOME/.config/i3
-    ln -s .vim $HOME/.config/nvim
-    ln -s .Xresources $HOME/.Xresources
-    ln -s .dmrc $HOME/.dmrc
-    for file in .config/* do
-        ln -s $file "${HOME}${file}"
-    done
-    ln -s .gitconfig $HOME/.gitconfig
-    ln -s .oh-my-zsh $HOME/.oh-my-zsh
-    ln -s .zshrc $HOME/.zshrc
-
-}
